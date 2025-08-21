9879c91d8bf34afa54850d02da062ff5d5e91887
LXSCA <lucas.edr27@gmail.com>
2025-08-21 00:46:34 -0300

    feat: volume change for all audible tabs

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,74 @@
+async function openPopUp() {
+   const tabsContainer = document.getElementById('tabs');
+   tabsContainer.innerHTML = '';
+
+   const audibleTabs = await browser.tabs.query({ audible: true });
+
+   if (audibleTabs.length === 0) {
+      tabsContainer.textContent = '0 tabs with sound.';
+      return;
+   }
+
+   for (const tab of audibleTabs) {
+      createTabItem(tab, tabsContainer);
+   };
+}
+
+async function createTabItem(tab, tabsContainer) {
+   const tabId = tab.id;
+
+   const results = await browser.scripting.executeScript({
+      target: {tabId: tab.id},
+      func: getActualVolume,
+   });
+
+   const volume = (results && results[0] && results[0].result != null) ? results[0].result : 1.0;
+   const tabItem = document.createElement('div');
+   tabItem.className = 'tab-item';
+
+   const icon = document.createElement('img');
+   icon.className = 'tab-icon';
+   icon.src = tab.favIconUrl || 'icon.png';
+
+   const info = document.createElement('div');
+   info.className = 'tab-info';
+   info.title = tab.title;
+   info.textContent = tab.title;
+
+   const slider = document.createElement('input');
+   slider.className = 'volume-slider';
+   slider.type = 'range';
+   slider.min = '0';
+   slider.max = '1';
+   slider.step = '0.05';
+   slider.value = volume;
+
+   slider.addEventListener('input', (event) => {
+      const newVolume = parseFloat(event.target.value);
+      
+      browser.scripting.executeScript({
+      target: { tabId: tabId },
+      func: setPageVolume,
+      args: [newVolume]
+      });
+   });
+
+   tabItem.appendChild(icon);
+   tabItem.appendChild(info);
+   tabItem.appendChild(slider);
+   tabsContainer.appendChild(tabItem);
+}
+
+document.addEventListener('DOMContentLoaded', openPopUp);
+
+function getActualVolume() {
+   const mediaElement = document.querySelector('video, audio');
+   if (mediaElement) {
+      return mediaElement.volume;
+   }
+   return 1.0;
+}
+
 function setPageVolume(volume) {
    document.querySelectorAll('video, audio').forEach(e => {
       e.volume = volume;
